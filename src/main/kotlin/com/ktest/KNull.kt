c563cfecf897a9c5846f1114faf883ce99a1e0a4
MB-teacher <vincenttuan@gmail.com>
2021-02-27 14:53:20 +0800

    Kotlin

--- src/main/kotlin/com/ktest/KNull.kt
+++ src/main/kotlin/com/ktest/KNull.kt
@@ -1,11 +1,14 @@
 package com.ktest
 
 fun main(args: Array<String>) {
-    val chinese: Int? = 100
+    val chinese: Int? = null
     val english: Int? = 90
     val math: Int? = 80
     // 若沒考則放入 null, 計算總分的時候以 0 分計算之
     // 請計算總分 = ?
-    val sum = 0
+    var sum = (chinese ?: 0) + (english ?: 0) + (math ?: 0)
     println(sum)
+    var sum2 = chinese?.plus(english ?: 0) ?: 0
+    println(sum2)
 }
+
