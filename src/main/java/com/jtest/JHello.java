c6055822daeb8f7b7ea0a74fb4ebce30d96b937a
MB-teacher <vincenttuan@gmail.com>
2021-02-27 12:23:21 +0800

    Kotlin

---
+++ src/main/java/com/jtest/JHello.java
@@ -0,0 +1,18 @@
+package com.jtest;
+
+import com.ktest.KHello;
+
+public class JHello {
+    public void callMe() {
+        System.out.println("0921443123");
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        // 呼叫 callMe()
+        JHello j = new JHello();
+        j.callMe();
+        // 呼叫 KHello 的 callMe
+        KHello k = new KHello();
+        k.callMe();
+    }
+}
---
+++ src/main/kotlin/com/ktest/KHello.kt
@@ -0,0 +1,18 @@
+package com.ktest
+
+import com.jtest.JHello
+
+class KHello {
+    fun callMe() {
+        print("pc@school.com")
+    }
+}
+
+fun main(args: Array<String>) {
+    println("Hello Kotlin")
+    // 呼叫 JHello 的 callMe
+    val j = JHello()
+    j.callMe()
+    val k = KHello()
+    k.callMe()
+}
