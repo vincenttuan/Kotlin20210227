102d41bc43490c63435b046eb0957505bfffdeea
MB-teacher <vincenttuan@gmail.com>
2021-03-06 14:15:49 +0800

    Kotlin

---
+++ src/main/kotlin/com/ktest/KArray.kt
@@ -0,0 +1,26 @@
+package com.ktest
+
+import java.util.*
+import kotlin.random.Random
+
+fun main() {
+    // 數組陣列
+    val num1 = arrayOf(1, 5, 7, 3) // 隱式
+    val num2 = arrayOf<Int>(1, 5, 7, 3) // 顯式
+    // for
+    for(i in 0..num1.size-1) {
+        println(num1[i])
+    }
+    println(num1.indices)
+    for(i in num1.indices) {
+        println(num1[i])
+    }
+    // 使用 lambda 創建
+    val num3 = Array(5) { i -> i * 1 }
+    for(i in num3.indices) {
+        println(num3[i])
+    }
+    // 四星彩電腦選號 (0~9)
+    val num4 = Array(4) { _ -> Random.nextInt(10) }
+    println(num4.contentToString()) // Arrays.toString(num4)
+}
