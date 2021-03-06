2d868203142ae0171592ecbcc3ab9e8888a2400d
MB-teacher <vincenttuan@gmail.com>
2021-03-06 10:34:30 +0800

    Kotlin

--- src/main/kotlin/com/lab/Elevator.kt
+++ src/main/kotlin/com/lab/Elevator.kt
@@ -4,6 +4,35 @@ package com.lab
 // B1, Lobby, 1, 2, 3, 4, 5, 6, 7, 空中花園
 // -1, 0, 1, 2, 3, 4, 5, 6, 7, 8
 fun main() {
+    // 電梯位置
+    var ePos = 0
+    do {
+        print("電梯在 ${ePos} 樓, 請輸入樓層(-1-8): ")
+        var floor = readLine()!!.toInt()
+        //if(floor < -1 || floor > 8) continue
+        // 處理樓層範圍錯誤
+        try {
+            floor = if(floor in -1..8) floor else throw Exception("樓層錯誤")
+        } catch (e: java.lang.Exception) {
+            println(e.message)
+            continue
+        }
 
 
+        if(floor > ePos) { // 電梯上樓
+            println("電梯上樓")
+            for(i in ePos..floor) {
+                Thread.sleep(1000)
+                println(i)
+            }
+        } else if(floor < ePos) { // 電梯下樓
+            println("電梯下樓")
+            for(i in ePos downTo floor) {
+                Thread.sleep(1000)
+                println(i)
+            }
+        }
+        // 電梯到達指定樓層
+        ePos = floor
+    } while (true)
 }
