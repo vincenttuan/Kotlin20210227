845b59983703811dcdbcb121891cf7d0aa1fcc7f
MB-teacher <vincenttuan@gmail.com>
2021-03-06 11:47:57 +0800

    Kotlin

---
+++ src/main/kotlin/com/ktest/Rect.kt
@@ -0,0 +1,21 @@
+package com.ktest
+
+class Rect(val h: Int, val w: Int) {
+    val area: Int = h * w
+    // 是否是矩形 ?
+    val isSquare: Boolean
+        get() {
+            return h == w
+        }
+
+    override fun toString(): String {
+        return "Rect(h: ${h}, w: ${w}, area: ${area}, isSquare: ${isSquare})"
+    }
+}
+
+fun main() {
+    val rect = Rect(10, 10)
+    println(rect.area)
+    println(rect.isSquare)
+    println(rect)
+}
