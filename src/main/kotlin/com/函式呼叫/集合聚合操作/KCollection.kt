177330fb5951c80269797e3aa1e8bc5b54ff9164
MB-teacher <vincenttuan@gmail.com>
2021-03-13 09:53:37 +0800

    Kotlin

--- src/main/kotlin/com/函式呼叫/集合聚合操作/KCollection.kt
+++ src/main/kotlin/com/函式呼叫/集合聚合操作/KCollection.kt
@@ -1,2 +1,14 @@
 package com.函式呼叫.集合聚合操作
 
+fun main() {
+    var numbers = listOf(6, 42, 10, 4)
+    println("Count: ${numbers.count()}")
+    println("Sum: ${numbers.sum()}")
+    println("Avg: ${numbers.average()}")
+    println("Max: ${numbers.max()}")
+    println("Min: ${numbers.min()}")
+    println("Max: ${numbers.maxByOrNull { it }}")
+    println("Min: ${numbers.minByOrNull { it }}")
+
+
+}
