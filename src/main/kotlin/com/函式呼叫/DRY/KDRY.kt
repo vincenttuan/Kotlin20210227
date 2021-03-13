af172049d32cd53a071410a897368d5b9ccd2672
MB-teacher <vincenttuan@gmail.com>
2021-03-13 12:26:35 +0800

    Kotlin

---
+++ src/main/kotlin/com/函式呼叫/DRY/KDRY.kt
@@ -0,0 +1,31 @@
+package com.函式呼叫.DRY
+
+// DRY (Don't Repeat Yourself)
+// 減少重複的程式碼
+
+data class User(val id:Int, val name:String, val address: String)
+
+// 撰寫一個可以驗證 User 的繼承函式
+fun User.validateBeforeSave() {
+    //println("${id}, ${name}, ${address}")
+    // 驗證
+    fun valid(value:String, fieldName: String) {
+        if(value.isEmpty()) {
+            throw Exception("Can't save user $id empty $fieldName")
+        }
+    }
+    valid(name, "Name")
+    valid(address, "address")
+}
+
+fun saveUser(user: User) {
+    println("Save $user")
+}
+
+fun main() {
+    val user = User(1, "Mary", "Taoyuan")
+    // 驗證是否每個欄位都有資料 ?
+    user.validateBeforeSave()
+    saveUser(user)
+}
+
