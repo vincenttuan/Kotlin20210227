827cbdd12ab2971098ef78d82196a8f4c7dd4d87
MB-teacher <vincenttuan@gmail.com>
2021-03-13 15:35:42 +0800

    Kotlin

--- src/main/kotlin/com/類別與物件/介面與抽象/KInterface.kt
+++ src/main/kotlin/com/類別與物件/介面與抽象/KInterface.kt
@@ -7,10 +7,28 @@ interface Clickable {
     }
 }
 
+interface Focusable {
+    fun setFocus(b: Boolean) // 一般抽象方法
+    fun showOff() { // 類似 java 介面的 default 方法
+        println("I am focusable!")
+    }
+}
+
 class Button: Clickable {
     override fun click() {
         println("Button clicked!")
     }
+}
+
+class TextField: Clickable, Focusable {
+    override fun click() {
+        println("TextField clicked!")
+    }
+
+    override fun setFocus(b: Boolean) {
+        println("I ${if(b) "got" else "lost"} focus.")
+    }
+
 }
 
 fun main() {
